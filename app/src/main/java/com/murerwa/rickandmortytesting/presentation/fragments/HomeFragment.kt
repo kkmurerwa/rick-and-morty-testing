215da61679c93af450efffce5c638d8cea42197e
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-09 11:09:48 +0300

    feat: Add dependencies

---
+++ app/src/main/java/com/murerwa/rickandmortytesting/presentation/fragments/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.murerwa.rickandmortytesting.presentation.fragments
+
+import androidx.fragment.app.Fragment
+import com.murerwa.rickandmortytesting.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
