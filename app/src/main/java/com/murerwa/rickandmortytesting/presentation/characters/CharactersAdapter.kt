ba4ac7186abab7dcb0fa5c8c5b62be6bf1378f82
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-13 21:07:37 +0300

    feat: Add ability to render character information

--- app/src/main/java/com/murerwa/rickandmortytesting/presentation/characters/CharactersAdapter.kt
+++ app/src/main/java/com/murerwa/rickandmortytesting/presentation/characters/CharactersAdapter.kt
@@ -1,10 +1,13 @@
 package com.murerwa.rickandmortytesting.presentation.characters
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.murerwa.rickandmortytesting.databinding.ListItemCharacterBinding
 import com.murerwa.rickandmortytesting.domain.models.characters.CharacterItem
+import com.murerwa.rickandmortytesting.utils.capitalizeString
+import com.murerwa.rickandmortytesting.utils.loadImage
 
 class CharactersAdapter(
     private val characters: List<CharacterItem>,
@@ -28,9 +31,23 @@ class CharactersAdapter(
 
     override fun getItemCount(): Int = characters.size
 
-    inner class ViewHolder(binding: ListItemCharacterBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ListItemCharacterBinding): RecyclerView.ViewHolder(binding.root) {
         fun bindView(character: CharacterItem) {
+            binding.apply {
+                imageView.loadImage(character.image)
 
+                textCharacterName.text = character.name
+                textCharacterLivingStatus.text = character.status.capitalizeString()
+                textCharacterSpecies.text = character.species
+
+                val color = when (character.status) {
+                    "Alive" -> Color.GREEN
+                    "unknown" -> Color.MAGENTA
+                    else -> Color.RED
+                }
+
+                textCharacterLivingStatus.setTextColor(color)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/murerwa/rickandmortytesting/utils/ViewExtensions.kt
@@ -0,0 +1,27 @@
+package com.murerwa.rickandmortytesting.utils
+
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.murerwa.rickandmortytesting.R
+
+fun ImageView.loadImage(
+    imageUrl: String,
+    customDrawable: Int = R.drawable.ic_launcher_background
+) {
+    // Create spinner drawable for the glide placeholder
+    val progressDrawable = CircularProgressDrawable(this.context)
+    progressDrawable.strokeWidth = 5f
+    progressDrawable.centerRadius = 30f
+    progressDrawable.start()
+
+    // Load images with glide
+    Glide.with(this.context)
+        .load(imageUrl)
+        .placeholder(progressDrawable)
+        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+        .error(customDrawable)
+        .fallback(customDrawable)
+        .into(this)
+}
