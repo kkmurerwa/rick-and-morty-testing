0752011be607f7e0c6f48631471b3c05fae48540
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-14 20:13:05 +0300

    feat: Add bottom nav and two extra screens for locations and episodes

--- app/src/main/java/com/murerwa/rickandmortytesting/MainActivity.kt
+++ app/src/main/java/com/murerwa/rickandmortytesting/MainActivity.kt
@@ -3,8 +3,9 @@ package com.murerwa.rickandmortytesting
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.R
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.R
+import androidx.navigation.ui.setupWithNavController
 import com.murerwa.rickandmortytesting.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(binding.navHostFragment.id) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
