4f43a1ab2e20e0c0d63f8317e406373ba8726859
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-15 16:30:43 +0300

    feat: Add location and episodes viewmodel

--- app/src/main/java/com/murerwa/rickandmortytesting/presentation/characters/CharactersViewModel.kt
+++ app/src/main/java/com/murerwa/rickandmortytesting/presentation/characters/CharactersViewModel.kt
@@ -1,11 +1,14 @@
 package com.murerwa.rickandmortytesting.presentation.characters
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.murerwa.rickandmortytesting.R
 import com.murerwa.rickandmortytesting.domain.models.common.ItemsResponse
 import com.murerwa.rickandmortytesting.data.network.NetworkResult
 import com.murerwa.rickandmortytesting.data.network.UIState
+import com.murerwa.rickandmortytesting.data.network.convertToUIState
 import com.murerwa.rickandmortytesting.domain.models.characters.Character
 import com.murerwa.rickandmortytesting.domain.repositories.CharactersRepository
 import com.murerwa.rickandmortytesting.utils.readError
@@ -14,39 +17,21 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class CharactersViewModel @Inject constructor(
+    private val app: Application,
     private val repository: CharactersRepository
 ) : ViewModel() {
     val charactersResponse = MutableLiveData<UIState<ItemsResponse<Character>>>(UIState.Loading)
 
-    fun getCharacters() {
-        viewModelScope.launch {
-            charactersResponse.value = UIState.Loading
+    fun getCharacters() = viewModelScope.launch {
+        charactersResponse.value = UIState.Loading
 
-            when (val response = repository.getCharacters(page = 1)) {
-                is NetworkResult.Success -> {
-                    charactersResponse.value = UIState.Success(response.value)
-                }
-                is NetworkResult.Failure -> {
-                    if (response.isNetworkError) {
-                        charactersResponse.value = UIState.Error("Network error")
-                    } else {
-                        if (response.errorBody != null) {
-                            Timber.d("Response is Not Null")
-                            val error = response.errorBody.readError()
-                            if (!error.isNullOrEmpty()) {
-                                charactersResponse.value = UIState.Error(error)
-                            } else {
-                                charactersResponse.value = UIState.Error("Error fetching characters")
-                            }
+        val uiState = convertToUIState(
+            response = repository.getCharacters(page = 1),
+            app = app,
+            errorMessage = app.getString(R.string.network_error_no_items_found)
+        )
 
-                        } else {
-                            Timber.d("Response is Null")
-                            charactersResponse.value = UIState.Error("Error fetching characters")
-                        }
-                    }
-                }
-            }
-        }
+        charactersResponse.value = uiState
     }
 
 }
