49d801924994fc5c18494f098cd347c52e56016d
Kenneth Murerwa <xwaxes@gmail.com>
2023-02-12 20:54:16 +0300

    fix: Fix failing CharactersViewModelShould test class

--- app/src/test/java/com/murerwa/rickandmortytesting/features/characters/presentation/CharactersViewModelShould.kt
+++ app/src/test/java/com/murerwa/rickandmortytesting/features/characters/presentation/CharactersViewModelShould.kt
@@ -1,21 +1,19 @@
 package com.murerwa.rickandmortytesting.features.characters.presentation
 
 import android.app.Application
+import com.murerwa.rickandmortytesting.R
 import com.murerwa.rickandmortytesting.core.models.Info
 import com.murerwa.rickandmortytesting.core.models.ItemsResponse
 import com.murerwa.rickandmortytesting.core.network.NetworkResult
 import com.murerwa.rickandmortytesting.core.network.UIState
-import com.murerwa.rickandmortytesting.core.network.convertToUIState
-import com.murerwa.rickandmortytesting.features.characters.domain.repositories.CharactersRepository
 import com.murerwa.rickandmortytesting.features.characters.domain.model.Character
+import com.murerwa.rickandmortytesting.features.characters.domain.repositories.CharactersRepository
 import com.murerwa.rickandmortytesting.utils.BaseUnitTest
-import com.murerwa.rickandmortytesting.utils.captureValues
 import com.murerwa.rickandmortytesting.utils.getValueForTest
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
@@ -26,16 +24,13 @@ class CharactersViewModelShould: BaseUnitTest() {
     private val app = mock<Application>()
     private val repository = mock<CharactersRepository>()
     private val mockInfo = mock<Info>()
-//    private val viewModel = CharactersViewModel(app, repository)
+    private val mockCharactersList = listOf<Character>(mock())
 
-//    private val mockNetworkResponse =
-//        mock<NetworkResult<ItemsResponse<Character>>>()
-//
     private val mockNetworkResponse =
-        NetworkResult.Success(ItemsResponse(mockInfo, listOf<Character>()))
+        NetworkResult.Success(ItemsResponse(mockInfo, mockCharactersList))
 
     private val mockLiveDataResponse =
-        mock<UIState<ItemsResponse<Character>>>()
+        UIState.Success(ItemsResponse(mockInfo, mockCharactersList))
 
     @Test
     fun getCharacters() = runTest {
@@ -43,36 +38,12 @@ class CharactersViewModelShould: BaseUnitTest() {
 
         viewModel.getCharacters()
 
-//        assertEquals(mockLiveDataResponse, values.last())
         assertEquals(mockLiveDataResponse, viewModel.charactersResponse.getValueForTest())
-
-//        viewModel.getPlaylistDetails(playlistId)
-//
-//        assertEquals(expected, viewModel.playlistDetails.getValueForTest())
-//
-//        viewModel.charactersResponse.captureValues {
-//            viewModel.charactersResponse.getValueForTest()
-//
-//            viewModel.getCharacters()
-//
-//            assertEquals(mockLiveDataResponse, values.last())
-//        }
     }
 
     private fun mockSuccessfulCase(): CharactersViewModel {
         runBlocking {
-            whenever(
-                convertToUIState(
-                    response = mockNetworkResponse,
-                    app = app,
-                    errorMessage = ""
-                )
-            ).thenReturn(
-                mockLiveDataResponse
-            )
-
-            whenever(app.getString(any())).thenReturn("")
-
+            whenever(app.getString(R.string.network_error_no_items_found)).thenReturn("test")
             whenever(repository.getCharacters(any())).thenReturn(mockNetworkResponse)
         }
 
